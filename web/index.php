0c7052ff35d39dbc82bc0d6246c7ffefc0adc682
jess2397 <eva15004@byui.edu>
2019-01-13 23:12:09 -0700

    Modified index.php and main.css

--- web/index.php
+++ web/index.php
@@ -76,8 +76,6 @@ function runMyFunction() {
     $txt = $_SERVER['REMOTE_ADDR'];
     fwrite($myfile, $txt);
     fclose($myfile);
-
-    echo '<p id="warning"> Please Do Not Touch the Animals! You have been recorded. Back Away Slowly.</p>'
 }
 
 if (isset($_GET['hello'])) {
