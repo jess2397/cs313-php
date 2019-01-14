86d7aa4af5e7d36ccb3a11c7651d94e85b58a6ac
jess2397 <eva15004@byui.edu>
2019-01-13 22:24:58 -0700

    added image files and main.css. updated index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,67 @@
-<?php
+<!doctype html>
+<html lang="en">
 
-  phpinfo();
+    <head>
+        <meta charset="utf-8">
+        <title>Fun "Facts" About Animals</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="main.css">
 
-?>
+    </head>
+
+    <body>
+        <div>
+            <header>
+                <h1><a href="#">Fun "Facts" About Animals</a></h1>
+                <hr>
+                <nav>
+                    <ul>
+                        <li><a href="#">Assignments</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <article>
+                    <section class="post-content">
+                        <h2>Mantis Shrimp</h2>
+                        <figure>
+                            <img src="mantis.jpg" alt="A Mantis Shrimp">
+                        </figure>
+                        <p><a href="#"></a></p>
+                    </section>
+                </article>
+
+                <article>
+                    <section class="post-content">
+                        <h2>Maned Wolf</h2>
+                        <figure>
+                            <img src="manedWolf.jpg" alt="Maned Wolf">
+                        </figure>
+                        <p><a href="#"></a></p>
+                    </section>
+                </article>
+
+                <article>
+                    <section class="post-content">
+                        <h2>Bearded Vulture</h2>
+                        <figure>
+                            <img src="bearded.jpg" alt="Bearded Vulture">
+                        </figure>
+                        <p><a href="#"></a></p>
+                    </section>
+                </article>
+
+                <article>
+                    <section class="post-content">
+                        <h2>Red Panda</h2>
+                        <figure>
+                            <img src="panda.jpg" alt="Red Panda">
+                        </figure>
+                        <p><a href="#"></a></p>
+                    </section>
+                </article>
+            </main>
+        </div>
+    </body>
+
+</html>
