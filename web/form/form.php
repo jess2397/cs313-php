8a5bbc9cea1770eec4a2ace55e50a56a7b6c9619
jess2397 <eva15004@byui.edu>
2019-01-22 10:30:14 -0700

    form TA

--- web/form/form.php
+++ web/form/form.php
@@ -5,7 +5,6 @@
         <meta charset="utf-8">
         <title>Form</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="shopping.css">
     </head>
 
     <body>
--- web/form/results.php
+++ web/form/results.php
@@ -6,12 +6,11 @@
         <meta charset="utf-8">
         <title>Form Results</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="shopping.css">
     </head>
 
     <body>
         Name: <?php echo $_POST["name"];?><br>
-        Email: <?php echo '<a href="mailto:' . $_POST["email"] . '>' . $_POST["email"] . '</a>'; ?><br>
+        Email: <?php echo '<a href="mailto:' . $_POST["email"] . '">' . $_POST["email"] . '</a>'; ?><br>
         Major: <?php echo $_POST["major"];?><br>
         Comments: <?php echo $_POST["comment"];?><br>
 
