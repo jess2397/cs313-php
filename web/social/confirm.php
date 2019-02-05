fc3bb0d23dbf5c252f032e289c0e4ef8243da317
jess2397 <eva15004@byui.edu>
2019-02-05 14:17:04 -0700

    add social site stuff

--- web/social/confirm.php
+++ web/social/confirm.php
@@ -23,18 +23,7 @@ catch (PDOException $ex)
     die();
 }
 
-$row = $db->query('SELECT * FROM author
-WHERE username =' . $_POST["username"]);
 
-if($_POST["pass"] == $row["password"])
-{
-    $_SESSION['user'] = $row["display_name"];
-    header('Location: /social/social.php');
-
-}
-else{
-    header('Location: /social/login.php?fail=true');
-}
 
 
 ?>
