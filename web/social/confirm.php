1ed41838bfddefb0116535a7903ea72a0206003b
jess2397 <eva15004@byui.edu>
2019-02-05 14:37:18 -0700

    add social site stuff

--- web/social/confirm.php
+++ web/social/confirm.php
@@ -1,7 +1,29 @@
 <?php
 session_start();
 
-foreach ($db->query('SELECT username, password FROM author WHERE username= ' . $_POST['username']) as $row)
+try
+{
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+}
+
+foreach ($db->query('SELECT username, password FROM author WHERE username= jess2397') as $row)
 {
     echo 'user: ' . $row['username'];
     echo ' password: ' . $row['password'];
