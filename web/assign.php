17191c04a50af4895b197471aabb7043405f399b
jess2397 <eva15004@byui.edu>
2019-02-05 15:09:27 -0700

    add social site stuff

--- web/assign.php
+++ web/assign.php
@@ -10,6 +10,7 @@
 
     <body>
         <h1><a href="/shopping/shopping.php">Assign 03: Shopping Cart</a></h1>
+        <h1><a href="/social/social.php">Assign 05: "Social Media"</a></h1>
     </body>
 
 </html>
